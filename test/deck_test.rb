6a1354a334fad0b9e2b1526700a012589767514b
Curtis Bartell <cbartell980@gmail.com>
2020-08-18 20:49:50 -0600

    Update deck_test.rb

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -1,14 +1,23 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/card'
-# require './lib/deck'
+require './lib/deck'
 
 class DeckTest < Minitest::Test
   def cards_setup
     card_1 = Card.new(:diamond, 'Queen', 12)
     card_2 = Card.new(:spade, '3', 3)
     card_3 = Card.new(:heart, 'Ace', 1)
+    cards = [card_1, card_2, card_3]
+  end
 
-    [card_1, card_2, card_3]
+  def test_it_exists
+    cards = cards_setup
+    deck = Deck.new(cards_setup)
+    assert_instance_of Deck, deck
+  end
+
+  def test_has_cards
+
   end
 end
