6666e2ddc86cadd25a89613fbc8622edb10f6c49
Curtis Bartell <cbartell980@gmail.com>
2020-08-19 19:08:57 -0600

    Add percent_high_ranking to class Deck

--- lib/deck.rb
+++ lib/deck.rb
@@ -14,4 +14,12 @@ class Deck
       card.rank >= 11
     end
   end
+
+  def percent_high_ranking
+    number_of_high_ranking_cards = high_ranking_cards.count.to_f
+    total_cards = cards.count.to_f
+
+    percent = (number_of_high_ranking_cards / total_cards) * 100
+    percent.round(2)
+  end
 end
