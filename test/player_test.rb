d586873b849139d2195d5e89e15a9c7bbfcfe7ec
Curtis Bartell <cbartell980@gmail.com>
2020-08-20 17:58:49 -0600

    Add #setup

--- test/player_test.rb
+++ test/player_test.rb
@@ -5,5 +5,11 @@ require './lib/deck'
 require './lib/player'
 
 class PlayerTest < Minitest::Test
-
+  def setup
+    @card_1 = Card.new(:diamond, 'Queen', 12)
+    @card_2 = Card.new(:spade, '3', 3)
+    @card_3 = Card.new(:heart, 'Ace', 14)
+    @deck = Deck.new([@card_1, @card_2, @card_3])
+    @player = Player.new('Clarissa', @deck)
+  end
 end
