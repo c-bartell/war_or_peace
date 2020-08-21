3eb425a4fa03850a95acce7850fafe98c2c90fe7
Curtis Bartell <cbartell980@gmail.com>
2020-08-20 20:30:49 -0600

    Add TurnTest class

---
+++ test/turn_test.rb
@@ -0,0 +1,10 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+require './lib/turn'
+
+def TurnTest < Minitest::Test
+
+end
