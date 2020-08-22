5597135f3a16c5df0ef66f655a3fea5297dcdadc
Curtis Bartell <cbartell980@gmail.com>
2020-08-22 13:58:46 -0600

    Add pile_cards

--- lib/turn.rb
+++ lib/turn.rb
@@ -32,9 +32,24 @@ class Turn
     end
   end
 
-  # def pile_cards
-  #
-  # end
+  def pile_cards
+    if type == :mutually_assured_destruction
+      3.times do
+        player1.deck.remove_card
+        player2.deck.remove_card
+      end
+    elsif type == :war
+      3.times do
+        @spoils_of_war << player1.deck.remove_card
+      end
+      3.times do
+        @spoils_of_war << player2.deck.remove_card
+      end
+    else
+      @spoils_of_war << player1.deck.remove_card
+      @spoils_of_war << player2.deck.remove_card
+    end
+  end
 
   # def award_spoils
   #
