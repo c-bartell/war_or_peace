9f3c8724047fc81b91e11750e1af4fa4bb292d81
Curtis Bartell <cbartell980@gmail.com>
2020-08-22 17:24:49 -0600

    Add setup and test_it_exists

--- test/game_test.rb
+++ test/game_test.rb
@@ -4,7 +4,14 @@ require './lib/card'
 require './lib/deck'
 require './lib/player'
 require './lib/turn'
+require './lib/game'
 
 class GameTest < Minitest::Test
+  def setup
+    @game = Game.new
+  end
 
+  def test_it_exists
+    assert_instance_of Game, game
+  end
 end
