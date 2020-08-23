a67b8585d8974ad5d0327f506927fb81bcbf491a
Curtis Bartell <cbartell980@gmail.com>
2020-08-22 18:39:18 -0600

    Add test_it_exists and test_standard_deck

--- test/game_test.rb
+++ test/game_test.rb
@@ -14,4 +14,24 @@ class GameTest < Minitest::Test
   def test_it_exists
     assert_instance_of Game, game
   end
+
+  def test_standard_deck
+    test_card_1 = Card.new(:spade, 'Ace', 14)
+    test_card_2 = Card.new(:heart, '4', 4)
+    test_card_3 = Card.new(:club, 'King', 13)
+    test_card_4 = Card.new(:diamond, '7', 7)
+
+    test_deck = game.standard_deck
+
+    assert_equal 52, test_deck.length
+
+    test_deck.each do |card|
+      assert_instance_of Card, card
+    end
+
+    assert_equal test_card_1, test_deck[12]
+    assert_equal test_card_1, test_deck[15]
+    assert_equal test_card_1, test_deck[37]
+    assert_equal test_card_1, test_deck[44]
+  end  
 end
